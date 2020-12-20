b840a7eca26b31e3a2f50abf8e47950868a8579c
your name your last name <soarnsoar@gmail.com>
2020-12-20 18:43:45 +0900

    fix process name

--- HighMassScalar/HighMassScalar.py
+++ HighMassScalar/HighMassScalar.py
@@ -49,9 +49,12 @@ class HighMassScalar(PhysicsModel):
                 120: 6.1841800000000005, 
                 250: 8.748479999999999, 
                 125: 8.731839999999998, 
-                126: 9.467639794273635}, 
+                126: 9.467639794273635, 
+                190: 15.53472,
+            }, 
             'VBF': {
                 130: 1.113035, 
+                175: 2.600401651328187,
                 900: 0.07127199999999999, 
                 5000: 8.080239999999999e-06, 
                 650: 0.15335159999999998, 
@@ -89,124 +92,75 @@ class HighMassScalar(PhysicsModel):
                 250: 1.169969, 
                 124: 0.7445578136647063, 
                 125: 0.8163999999999999, 
-                126: 0.8907425606102282
+                126: 0.8907425606102282,
+                190:1.91052,
+                135:1.422568,
             }
         }
         HWWLNUQQ_XSEC={
             'GGF': {
-                130: 5.071637916827999, 
-                900: 0.061046245666224, 
-                135: 6.3001933692768, 
-                5000: 3.3711943229999994e-08, 
-                650: 0.33493551331967997, 
-                140: 7.4081764645056, 
-                270: 3.4056485878464, 
-                400: 2.4075888289689598, 
-                145: 8.356296866072398, 
-                150: 9.153624953491198, 
-                155: 10.075334101420307, 
-                160: 10.697298128319598, 
-                165: 10.65537606189697, 
-                550: 0.72139956715008, 
-                170: 10.2321655528932, 
-                300: 2.98145043750708, 
-                175: 9.698813871583427, 
-                180: 8.921563795871998, 
-                3000: 9.4447380153168e-06, 
-                700: 0.23283817947683996, 
-                800: 0.11638592684964, 
-                450: 1.62386850703896, 
-                4000: 5.170063613752799e-07, 
-                2500: 4.7951868050352006e-05, 
-                200: 5.8817699540207995, 
-                2000: 0.00029234997169055995, 
-                210: 5.272965202553999, 
-                600: 0.48902303265479996, 
-                1500: 0.0024037964000719197, 
-                350: 3.0345141342599997, 
-                230: 4.428374508079199, 
-                1000: 0.033253460802072, 
-                750: 0.163558313278416, 
-                115: 1.7772404986148396, 
-                500: 1.07836841592648, 
-                120: 2.7163612388808, 
-                250: 3.8427134997887995, 
-                125: 3.835404486950399, 
-                126: 4.158599808034419}, 
+                130: 3.3810919445520002, 
+                900: 0.04069749711081601, 
+                135: 4.200128912851201, 
+                5000: 2.247462882e-08, 
+                650:0.22329034221312002,
+                140: 4.938784309670401, 
+                270: 2.2704323918976, 
+                400: 1.6050592193126403, 
+                145: 5.5708645773816, 
+                150: 6.1024166356608, 
+                155: 6.716889400946872, 
+                160: 7.131532085546401, 
+                165: 7.103584041264648, 
+                550: 0.4809330447667201, 
+                170: 6.821443701928801, 
+                300: 1.9876336250047202, 175: 6.465875914388953,180: 5.947709197248001, 3000: 6.296492010211201e-06, 700: 0.15522545298456, 190: 4.5490171442688005, 800: 0.07759061789976002, 450: 1.0825790046926402, 4000: 3.4467090758352003e-07, 2500: 3.1967912033568006e-05, 200: 3.9211799693472003, 2000: 0.00019489998112704002, 210: 3.515310135036, 600: 0.3260153551032, 1500: 0.0016025309333812803, 350: 2.0230094228400004, 230: 2.9522496720528, 1000: 0.022168973868048003, 750: 0.10903887551894402, 115: 1.1848269990765599, 500: 0.7189122772843202, 120: 1.8109074925872004, 250: 2.5618089998592, 124: 2.3466716894041206, 125: 2.5569363246336, 126: 2.7723998720229464
+            },
+            
             'VBF': {
-                130: 0.4888934558046, 
-                900: 0.031305767008319996, 
-                5000: 3.5491933832543992e-06, 
-                650: 0.06735870271569598, 
-                140: 0.75464854766784, 
-                270: 0.4539845738736, 
-                400: 0.19177725224448, 
-                145: 0.8727514775935199, 
-                150: 0.9788244048979199, 
-                155: 1.1014735380563145, 
-                160: 1.1938771733867999, 
-                165: 1.2123460924030072, 
-                550: 0.09533122604460001, 
-                170: 1.18522231828056, 
-                300: 0.38121772874975995, 
-                180: 1.0669928862096, 
-                3000: 0.00022257973398175196, 
-                700: 0.0573586202826, 
-                190: 0.8391836062511999, 
-                800: 0.04203473020488, 
-                450: 0.144807532794288, 
-                4000: 2.8345001867783997e-05, 
-                2500: 0.0006364814881823999, 
-                200: 0.7407394610968799, 
-                2000: 0.00190189298926368, 
-                210: 0.6770930692528799, 
-                600: 0.07981362955692001, 
-                1500: 0.0061706340888192, 
-                350: 0.286162084114704, 
-                230: 0.5850671509972799, 
-                1000: 0.023549815062748797, 
-                750: 0.04895501249267999, 
-                115: 0.15531235000218, 
-                500: 0.115773709175712, 
-                120: 0.24588063850392003, 
-                250: 0.51390134864964, 
-                124: 0.32704222469990224, 
-                125: 0.35859844238399996, 
-                126: 0.39125293336595235
+                130: 0.3259289705364, 900: 0.02087051133888, 135: 0.41656922177472006, 5000: 2.3661289221696e-06, 650: 0.044905801810464, 140: 0.5030990317785601, 270: 0.3026563825824001, 400: 0.12785150149632002, 145: 0.5818343183956801, 150: 0.65254960326528, 160: 0.7959181155912001, 165: 0.8082307282686716, 550: 0.06355415069640002, 170: 0.79014821218704, 300: 0.25414515249984, 175: 0.7614731191728479, 180:0.7113285908064002, 3000: 0.000148386489321168, 700: 0.038239080188400006, 190: 0.5594557375008, 800: 0.028023153469920004, 450: 0.09653835519619203, 4000: 1.8896667911856e-05, 2500: 0.00042432099212160005, 200: 0.4938263073979201, 2000: 0.0012679286595091203, 210: 0.45139537950192, 600: 0.053209086371280016, 1500: 0.0041137560592128, 350: 0.19077472274313606, 230: 0.39004476733152005, 1000: 0.015699876708499202, 750: 0.032636674995120005, 115: 0.10354156666812, 500: 0.077182472783808, 120: 0.16392042566928006, 250: 0.34260089909976005, 124: 0.21802814979993485, 125: 0.239065628256, 126: 0.26083528891063495,155:0.7343156920375432,
             }
         }
         
+        
         mass=int(self.mass)
-        print "mass=",mass
+        #print "mass=",mass
         self.xsec_ggHlnuqq_sm=HWWLNUQQ_XSEC['GGF'][mass]
         self.xsec_qqHlnuqq_sm=HWWLNUQQ_XSEC['VBF'][mass]
         self.xsec_ggH_sm=HWW_XSEC['GGF'][mass]
         self.xsec_qqH_sm=HWW_XSEC['VBF'][mass]
-        print "SM Xsec ggHlnuqq=",self.xsec_ggHlnuqq_sm
-        print "SM Xsec qqHlnuqq=",self.xsec_qqHlnuqq_sm
-        print "SM Xsec ggH=",self.xsec_ggH_sm
-        print "SM Xsec qqH=",self.xsec_qqH_sm
+        #print "SM Xsec ggHlnuqq=",self.xsec_ggHlnuqq_sm
+        #print "SM Xsec qqHlnuqq=",self.xsec_qqHlnuqq_sm
+        #print "SM Xsec ggH=",self.xsec_ggH_sm
+        #print "SM Xsec qqH=",self.xsec_qqH_sm
     def doParametersOfInterest(self):
-        print "<doParametersOfInterest>"
+        #print "<doParametersOfInterest>"
         """Create POI out of signal strength """
         self.SetMass()
-        self.modelBuilder.doVar("sigma[0,0,10]") ##mu is what we want to return (in string) name[starting_value,min,max] 
-        self.modelBuilder.doVar("fvbf[0,0,1]") ##mu is what we want to return (in string) name[starting_value,min,max] 
-        
-        #self.modelBuilder.factory_( 'expr::r_ggH(\"@0*(1-@1)'+'/'+str(self.xsec_ggHlnuqq_sm)+'\", sigma,fvbf)')
-        #self.modelBuilder.factory_( 'expr::r_qqH(\"@0*@1'+'/'+str(self.xsec_qqHlnuqq_sm)+'\", sigma,fvbf)')
-        self.modelBuilder.factory_( 'expr::r_ggH(\"@0*(1-@1)'+'/'+str(self.xsec_ggH_sm)+'\", sigma,fvbf)')
-        self.modelBuilder.factory_( 'expr::r_qqH(\"@0*@1'+'/'+str(self.xsec_qqH_sm)+'\", sigma,fvbf)')
+        if not self.sigstrength:
+            self.modelBuilder.doVar("sigma[0,0,10]") ##mu is what we want to return (in string) name[starting_value,min,max] 
+            self.modelBuilder.doVar("fvbf[0,0,1]") ##mu is what we want to return (in string) name[starting_value,min,max] 
+            self.modelBuilder.factory_( 'expr::r_ggH(\"@0*(1-@1)'+'/'+str(self.xsec_ggH_sm)+'\", sigma,fvbf)')
+            self.modelBuilder.factory_( 'expr::r_qqH(\"@0*@1'+'/'+str(self.xsec_qqH_sm)+'\", sigma,fvbf)')
+            
+            #self.modelBuilder.doSet("POI","sigma,fvbf")
 
-        print 'expr::r_ggH(\"@0*(1-@1)'+'/'+str(self.xsec_ggHlnuqq_sm)+'\", sigma,fvbf)'
-        print 'expr::r_qqH(\"@0*@1'+'/'+str(self.xsec_qqHlnuqq_sm)+'\", sigma,fvbf)'
-        #self.modelBuilder.factory_('expr::r_ggH(\"@0*(1-@1)\", r,fvbf)')
-        #self.modelBuilder.factory_('expr::r_qqH(\"@0*@1\", r,fvbf)')
+
+        else:
+            self.modelBuilder.doVar("r[0,0,10]") ##r is what we want to return (in string) name[starting_value,min,max] 
+            self.modelBuilder.factory_( 'expr::r_ggH(\"@0\",r)')
+            self.modelBuilder.factory_( 'expr::r_qqH(\"@0\",r)')            
+            #self.modelBuilder.doSet("POI","r")
+
+            #print 'expr::r_ggH(\"@0*(1-@1)'+'/'+str(self.xsec_ggHlnuqq_sm)+'\", sigma,fvbf)'
+            #print 'expr::r_qqH(\"@0*@1'+'/'+str(self.xsec_qqHlnuqq_sm)+'\", sigma,fvbf)'
+            #self.modelBuilder.factory_('expr::r_ggH(\"@0*(1-@1)\", r,fvbf)')
+            #self.modelBuilder.factory_('expr::r_qqH(\"@0*@1\", r,fvbf)')
         if self.noInterference:
             self.modelBuilder.factory_('expr::ggH_s_func(\"@0\", r_ggH)')
             self.modelBuilder.factory_( 'expr::ggH_b_func(\"1\", r_ggH)')
             self.modelBuilder.factory_( 'expr::ggH_sbi_func(\"0\", r_ggH)')
-
+            
             self.modelBuilder.factory_('expr::qqH_s_func(\"@0\", r_qqH)')
             self.modelBuilder.factory_( 'expr::qqH_b_func(\"1\", r_qqH)')
             self.modelBuilder.factory_( 'expr::qqH_sbi_func(\"0\", r_qqH)')
@@ -214,53 +168,60 @@ class HighMassScalar(PhysicsModel):
             self.modelBuilder.factory_('expr::ggH_s_func(\"@0-sqrt(@0)\", r_ggH)')
             self.modelBuilder.factory_( 'expr::ggH_b_func(\"1-sqrt(@0)\", r_ggH)')
             self.modelBuilder.factory_( 'expr::ggH_sbi_func(\"sqrt(@0)\", r_ggH)')
-
+            
             self.modelBuilder.factory_('expr::qqH_s_func(\"@0-sqrt(@0)\", r_qqH)')
             self.modelBuilder.factory_( 'expr::qqH_b_func(\"1-sqrt(@0)\", r_qqH)')
             self.modelBuilder.factory_( 'expr::qqH_sbi_func(\"sqrt(@0)\", r_qqH)')
         
-        
-        self.modelBuilder.doSet("POI","sigma,fvbf")
+        if not self.sigstrength:
+            self.modelBuilder.doSet("POI","sigma,fvbf")
+        else:
+            self.modelBuilder.doSet("POI","r")
 
     def setPhysicsOptions(self,physOptions):
-        print "<setPhysicsOptions>"
+        #print "<setPhysicsOptions>"
+        print "physOptions=",physOptions
         self.noInterference=False
-        
+        self.sigstrength=False
         for po in physOptions:
-            if 'KillInterference'==po:
+            if 'KillInterference' in po:
                 self.noInterference=True
-
-        
+            if 'SignalStrength' in po:
+                self.sigstrength=True
+        print "self.noInterference",self.noInterference
+        print "self.sigstrength",self.sigstrength
     def getYieldScale(self,bin,process): ##bin process in datacard
-        print "<getYieldScale>"
-        ##print "self.options.mass=",self.options.mass :: get this value using -m
+        #print "<getYieldScale>"
+        ###print "self.options.mass=",self.options.mass :: get this value using -m
         mass=int(self.mass)
         #if  "ggHWW" in process:
-        print process
+        #print process
         ##--ggH
-        if  "ggHWWlnuqq_M"+str(mass) in process:
+        #if  "ggHWWlnuqq_M"+str(mass) in process:
+        if  "ggH_hww"+str(mass) in process:
             if 'SBI' in process:
-                print "->ggH_sbi_func"
+                #print "->ggH_sbi_func"
                 return "ggH_sbi_func"
             elif 'S' in process:
-                print "->ggH_s_func"
+                #print "->ggH_s_func"
                 return "ggH_s_func"
         if 'ggWW' in process:
-            print "->ggH_b_func"
+            #print "->ggH_b_func"
             return 'ggH_b_func'
 
         ##--qqH
-        if "vbfHWWlnuqq_M"+str(mass) in process:
+        #if "vbfHWWlnuqq_M"+str(mass) in process:
+        if "qqH_hww"+str(mass) in process:
             if 'SBI' in process:
-                print "->qqH_sbi_func"
+                #print "->qqH_sbi_func"
                 return "qqH_sbi_func"
             elif 'S' in process:
-                print '->qqH_s_func'
+                #print '->qqH_s_func'
                 return 'qqH_s_func'
         if 'qqWWqq' in process:
-            print '->qqH_b_func'
+            #print '->qqH_b_func'
             return 'qqH_b_func'
-        print "->1"
+        #print "->1"
         return 1
 
         #self.modelBuilder.doSet("POI","r_qqH")
